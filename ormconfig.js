a0e74f42bcd9cbb3785a3b3754b7c258a5a0874b
Wojciech Dudek <wdudek82@gmail.com>
2021-08-15 17:39:07 +0200

    Refactor ormconfig.js

--- ormconfig.js
+++ ormconfig.js
@@ -1,9 +1,31 @@
-module.exports = {
-  type: 'sqlite',
-  database: 'db.sqlite',
-  entities:
-    process.env.NODE_ENV === 'development'
-      ? ['**/*.entity.js']
-      : ['**/*.entity.js'],
+var dbConfig = {
   synchronize: false,
 };
+
+switch (process.env.NODE_ENV) {
+  case 'development':
+    Object.assign(dbConfig, {
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: ['**/*.entity.js'],
+    });
+    break;
+  case 'test':
+    Object.assign(dbConfig, {
+      type: 'sqlite',
+      database: 'test.sqlite',
+      entities: ['**/*.entity.ts'],
+    });
+    break;
+  case 'production':
+    // Object.assign(dbConfig, {
+    //   type: 'sqlite',
+    //   database: 'db.sqlite',
+    //   entities: ['**/*.entity.js'],
+    // });
+    break;
+  default:
+    throw new Error('Unknown environment');
+}
+
+module.exports = dbConfig;
